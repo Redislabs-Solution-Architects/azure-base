eaa37dbc439cb2d624d1e49b3f33171da099f1fd
DaShaun <dashaun@redislabs.com>
2021-03-28 15:07:50 -0500

    fix output

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,3 @@
-locals {
-  redisgeek_config = {
-    sp = data.azuread_service_principal.redisgeek.display_name
-  }
-}
-
-output "redisgeek_config" {
-  value     = jsonencode(local.redisgeek_config)
-  sensitive = false
+output "sp" {
+  value = data.azuread_service_principal.redisgeek.display_name
 }
